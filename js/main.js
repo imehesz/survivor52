d7e2566d219e418cc0dc5d77145d33ab508a39ce
Imre Mehesz <imehesz@gmail.com>
2025-04-07 09:48:02 -0400

    Center everything

--- js/main.js
+++ js/main.js
@@ -1,21 +1,20 @@
-// js/main.js
-
+// Inside js/main.js
 const gameConfig = {
     type: Phaser.AUTO,
     scale: {
         mode: Phaser.Scale.FIT, // Fit to window keeping aspect ratio
-        parent: 'game-container',
-        autoCenter: Phaser.Scale.CENTER_BOTH,
+        parent: 'game-container', // Target the container div
+        autoCenter: Phaser.Scale.CENTER_BOTH, // Tell Phaser to center within the parent
         width: 480,  // Portrait aspect ratio
         height: 800
     },
     backgroundColor: '#ffffff', // White background for the game area
     scene: [
-        WelcomeScene, // Start with Welcome
+        WelcomeScene,
         HowToPlayScene,
         AboutScene,
         GameScene,
-        UIScene,      // UI runs in parallel with GameScene
+        UIScene,
         GameOverScene
     ]
 };
